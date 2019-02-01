04f4fe5539ac4566fb08ad797f16356a55cccb00
dhy <1101527438@qq.com>
2019-02-01 22:06:14 +0800

    测试数据

--- org/apache/mina/core/buffer/AbstractIoBuffer.java
+++ org/apache/mina/core/buffer/AbstractIoBuffer.java
@@ -48,6 +48,7 @@ public abstract class AbstractIoBuffer extends IoBuffer {
 		this.recapacityAllowed = true;
 		this.derived = false;
 		this.minimumCapacity = initialCapacity;
+		int i = 1+2;
 	}
 
 	protected AbstractIoBuffer(AbstractIoBuffer parent) {
